cbbccb5c98c22554531b51aff92f572337f38d74
kvadeck <kvadeck@yandex.ru>
2021-04-12 14:40:13 +0300

    feat: gh-pages add

--- src/js/modules/Game.js
+++
@@ -1,4 +0,0 @@
-export default function Game() {
-  const game = document.querySelector('#root');
-  game.insertAdjacentHTML('beforeend', '<main class="game"></main>');
-}
--- src/js/modules/Menu.js
+++
@@ -1,4 +0,0 @@
-export default function Menu() {
-  const game = document.querySelector('.game');
-  game.insertAdjacentHTML('beforeend', '<section class="menu"></section>');
-}
